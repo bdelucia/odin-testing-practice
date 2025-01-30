faaf2d35ba4f34c45c45f8ffe84135c57dedb93e
Robert DeLucia Jr. <bdelucia@asu.edu>
2025-01-29 17:22:30 -0700

    jest config

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
-console.log('Hello, Webpack!');
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+
+function calculator(a, b, operator) {
+  switch (operator) {
+    case '+':
+      return a + b;
+    case '-':
+      return a - b;
+    case '*':
+      return a * b;
+    case '/':
+      return a / b;
+  }
+}
+
+function caesarCipher(str, shift) {
+  return str
+    .split('')
+    .map((char) => {
+      const code = char.charCodeAt(0);
+      if (code >= 65 && code <= 90) {
+        return String.fromCharCode(((code - 65 + shift) % 26) + 65);
+      } else if (code >= 97 && code <= 122) {
+        return String.fromCharCode(((code - 97 + shift) % 26) + 97);
+      } else {
+        return char;
+      }
+    })
+    .join('');
+}
+
+function analyzeArray(arr) {
+  return {
+    average: arr.reduce((acc, curr) => acc + curr, 0) / arr.length,
+    min: Math.min(...arr),
+    max: Math.max(...arr),
+    length: arr.length,
+  };
+}
